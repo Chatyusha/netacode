94e42cd0f58c5f1fae4bff968cfc69fbe05877e8
Chatyusha <hirounicat76@gmail.com>
2022-01-17 14:40:00 +0000

    FC

---
+++ C++/LikePython.cpp
@@ -0,0 +1,18 @@
+/*
+Author : Chatyusha
+License : MIT
+*/
+
+#include <iostream>
+
+#define for for(int 
+#define in =0;i<
+#define range(n) n;i++){
+#define print(x) std::cout<<i<<std::endl;}
+
+int main(int argc, char const *argv[])
+{
+    for i in range(3)
+        print(i)
+    return 0;
+}
---
+++ C/Bible.c
@@ -0,0 +1,50 @@
+/*
+Author : Chatyusha
+License : MIT
+*/
+
+#include "stdio.h"
+
+#define and ;
+#define be return 0
+#define beginning a
+#define created = 
+#define Darkness (int i
+#define deep i
+#define earth 
+#define empty for
+#define formless = 5
+#define God 
+#define God_s <=
+#define heavens 3
+#define hovering ){
+#define In int
+#define Let ,i);}
+#define light 
+#define Now int b
+#define of ;
+#define on =
+#define over int x=
+#define said "i=%d\n"
+#define Spirit 3;i++
+#define surface 0
+#define the 
+#define there 
+#define was 
+#define waters printf(
+
+int main(){
+
+    /*
+    In the beginning God created the heavens and the earth.
+    Now the earth was formless and empty. 
+    Darkness was on the surface of the deep. 
+    God's Spirit was hovering over the surface of the waters. 
+    God said, "Let there be light," and there was light. 
+    */
+    In the beginning God created the heavens and the earth
+    Now the earth was formless and empty 
+    Darkness was on the surface of the deep 
+    God_s Spirit was hovering over the surface of the waters 
+    God said Let there be light and there was light
+}
